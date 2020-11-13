3a6025d49c33f55018315edb52e2b620c44118d8
Anish Thomas <anish.thomas@evolvussolutions.com>
2020-11-13 17:32:26 +0530

    fixed the rest of the numbers

--- containers/create-source-environment/setup.sql
+++ containers/create-source-environment/setup.sql
@@ -2,11 +2,11 @@ DROP TABLE NAME_LIST;
 CREATE TABLE NAME_LIST ( id bigint, state varchar(2), gender varchar(1), year varchar(4), name varchar(255), usage bigint); 
 DROP TABLE EMP_LIST;
 CREATE TABLE EMP_LIST (
-   EMPNO  NUMBER(5),
+   EMPNO  bigint(5),
    NAME VARCHAR(20),
    JOB  VARCHAR(10),
-   MGR  NUMBER(5),
-   SAL  NUMBER(10,2),
-   COMM NUMBER(10,2),
-   DEPTNO NUMBER(3) 
+   MGR  bigint(5),
+   SAL  bigint(10,2),
+   COMM bigint(10,2),
+   DEPTNO bigint(3) 
 );

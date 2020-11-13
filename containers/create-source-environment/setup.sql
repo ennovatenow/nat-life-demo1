3759c41a042c8be5c07c7d9a41a626b765035ad0
Anish Thomas <anish.thomas@evolvussolutions.com>
2020-11-13 17:38:55 +0530

    bigint changes

--- containers/create-source-environment/setup.sql
+++ containers/create-source-environment/setup.sql
@@ -2,11 +2,11 @@ DROP TABLE NAME_LIST;
 CREATE TABLE NAME_LIST ( id bigint, state varchar(2), gender varchar(1), year varchar(4), name varchar(255), usage bigint); 
 DROP TABLE EMP_LIST;
 CREATE TABLE EMP_LIST (
-   EMPNO  bigint(5),
+   EMPNO  bigint,
    NAME VARCHAR(20),
    JOB  VARCHAR(10),
-   MGR  bigint(5),
-   SAL  bigint(10,2),
-   COMM bigint(10,2),
-   DEPTNO bigint(3) 
+   MGR  bigint,
+   SAL  bigint,
+   COMM bigint,
+   DEPTNO bigint 
 );

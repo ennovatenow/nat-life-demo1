08a0ce8f3daf5286234756410cc201a3d7f65da9
Anish Thomas <anish.thomas@evolvussolutions.com>
2020-11-13 18:10:48 +0530

    number changes

--- containers/create-masked-environment/setup.sql
+++ containers/create-masked-environment/setup.sql
@@ -2,7 +2,7 @@ DROP TABLE MASKED_NAME_LIST;
 CREATE TABLE MASKED_NAME_LIST ( id bigint, state varchar(2), gender varchar(1), year varchar(4), name varchar(255), usage bigint); 
 DROP TABLE MASKED_EMP_LIST;
 CREATE TABLE MASKED_EMP_LIST (
-   EMPNO  bigint(5),
+   EMPNO  bigint,
    NAME VARCHAR(20),
    JOB  VARCHAR(10),
    MGR  bigint
